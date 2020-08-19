54b2ae5ad962ad1332693d96b297de199e03ab6a
Nolan Phillips <ncphillips.19@gmail.com>
2020-08-19 18:04:45 -0300

    switch to typescript and setup basic prisma API example

---
+++ pages/api/users.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+export default (req, res) => {
+  const prisma = new PrismaClient();
+  listUsers(prisma)
+    .then((users) => {
+      res.statusCode = 200;
+      res.json({ users });
+    })
+    .catch(() => {
+      res.statusCode = 500;
+      res.json({ message: 'Uh oh' });
+    })
+    .finally(() => {
+      prisma.$disocnnect();
+    });
+};
+
+async function listUsers(prisma: PrismaClient) {
+  return prisma.user.findMany();
+}
