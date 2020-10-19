09075e093bab2c1732cbab993d012e714e7e78f0
ncphillips <nolan@ncphi.com>
2020-10-19 17:26:07 -0300

    add plausible to website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
-import '@app/styles/globals.css';
-import { Provider as AuthProvider } from 'next-auth/client';
+import "@app/styles/globals.css";
+import { Provider as AuthProvider } from "next-auth/client";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <script
+          async
+          defer
+          data-domain="gmkit.vercel.app"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
